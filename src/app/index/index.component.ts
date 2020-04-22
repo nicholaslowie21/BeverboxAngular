98265296fe788f9dfc6007b2d1e2c587ee1f6912
unknown <vennyl256@gmail.com>
2020-04-22 17:46:32 +0800

    style:index & header

--- src/app/index/index.component.ts
+++ src/app/index/index.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { SessionService } from '../session.service';
+import {MenuItem} from 'primeng/api';
 
 @Component({
   selector: 'app-index',
@@ -8,12 +9,19 @@ import { SessionService } from '../session.service';
 })
 export class IndexComponent implements OnInit {
 
+  steps:MenuItem[]
+
   constructor(public sessionService: SessionService)
 	{
 	}
 
   ngOnInit() {
-
+    this.steps= [
+      {label: 'Register & Log In'},
+      {label: 'Browse'},
+      {label: 'Customise & Pay'},
+      {label: 'Delivered at your doorstep'},
+    ];
   }
 
 }
